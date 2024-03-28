437f3f1b9e351778e13b3f9c5b243e97ae0fda53
Kapil Arora <2754963a@student.gla.ac.uk>
2024-03-28 06:02:53 +0000

    Add / Remove an item from a list and display it.

---
+++ src/components/Eight.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+function Eight() {
+
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = () => {
+        if (input.trim() !== '') {
+            setTodos([...todos, input]);
+            console.log(todos);
+            setInput('');
+        }
+    }
+
+    const removeTodo = (index) => {
+        const updatedTodos = todos.filter((_, i) => i !== index);
+        setTodos(updatedTodos);
+    }
+
+    return (
+        <div>
+            <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo, index) =>
+                    <li key={index}>
+                        {todo} <button onClick={() => removeTodo(index)}>Remove</button>
+                    </li>
+                )}
+
+            </ul>
+        </div>
+    )
+}
+
+export default Eight
